4ea46e9e739f441089c8c711a44a392ae2ccab9c
0404233 <andrey0404233@gmail.com>
2025-01-07 01:06:57 +0300

    feat: done vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,15 +20,67 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let encrypted = '';
+    let keyIndex = 0;
+
+    for (let char of message) {
+      if (alphabet.includes(char)) {
+        const messageIndex = alphabet.indexOf(char);
+        const keyCharIndex = alphabet.indexOf(key[keyIndex]);
+        const shift = (messageIndex + keyCharIndex) % 26;
+        encrypted += alphabet[shift];
+        keyIndex = (keyIndex + 1) % key.length;
+      } else {
+        encrypted += char;
+      }
+    }
+
+    return this.isDirect ? encrypted : encrypted.split('').reverse().join('');
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) {
+      throw new Error('Incorrect arguments!');
+
+    }
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+
+    let decrypted = '';
+    let keyIndex = 0;
+
+    for (let char of encryptedMessage) {
+      if (alphabet.includes(char)) {
+        const encryptedIndex = alphabet.indexOf(char);
+        const keyCharIndex = alphabet.indexOf(key[keyIndex]);
+        const shift = (encryptedIndex - keyCharIndex + 26) % 26;
+        decrypted += alphabet[shift];
+        keyIndex = (keyIndex + 1) % key.length;
+      } else {
+        decrypted += char;
+      }
+    }
+
+    return this.isDirect ? decrypted : decrypted.split('').reverse().join('');
   }
 }
+
+
+
 
 module.exports = {
   VigenereCipheringMachine
